81650e3ae83308ec1b235eb2dd7222d0eaeb19ab
sanjipun <iamsanjipun@gmail.com>
2022-03-24 11:53:16 +0545

    add logos

--- src/views/HomePage/FindUs.tsx
+++ src/views/HomePage/FindUs.tsx
@@ -1,5 +1,8 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter';
+import { FaTelegram } from '@react-icons/all-files/fa/FaTelegram';
+import { FaDiscord } from '@react-icons/all-files/fa/FaDiscord';
 
 const FindUs: React.FC = () => {
   return (
@@ -8,8 +11,11 @@ const FindUs: React.FC = () => {
         <h1 className="py-10 text-4xl text-center font-audiowide gradient-text">Find US</h1>
         <div className="max-w-lg mx-auto place-items-center grid grid-cols-3 gap-10">
           {SOCIALS.map((social) => (
-            <Link key={social.title} to={social.link}>
-              {social.title}
+            <Link
+              key={social.title}
+              to={social.link}
+              className="text-2xl flex flex-col items-center justify-center">
+              {social.icon} {social.title}
             </Link>
           ))}
         </div>
@@ -20,8 +26,8 @@ const FindUs: React.FC = () => {
 
 export default FindUs;
 
-const SOCIALS: { title: string; link: string }[] = [
-  { title: 'Twitter', link: 'https://twitter.com' },
-  { title: 'Telegram', link: 'https://telegram.com' },
-  { title: 'Discord', link: 'https://discord.com' },
+const SOCIALS: { title: string; link: string; icon: React.ReactNode }[] = [
+  { title: 'Twitter', link: 'https://twitter.com', icon: <FaTwitter /> },
+  { title: 'Telegram', link: 'https://telegram.com', icon: <FaTelegram /> },
+  { title: 'Discord', link: 'https://discord.com', icon: <FaDiscord /> },
 ];
